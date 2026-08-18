65755d7a3cfac3031ee260d8748c5d38a9cea4ba
Matheus Vinicius Gabriel <matheusvinic55@gmail.com>
2026-08-18 01:44:57 -0300

    Restaura fundo cinematográfico da tela inicial

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,10 @@
-const CACHE_NAME = "a-lei-do-amor-v7";
+const CACHE_NAME = "a-lei-do-amor-v8";
 const CORE_ASSETS = [
   "/",
   "/manifest.webmanifest",
   "/favicon.svg",
   "/app-icon.svg",
   "/home-hero-background.webp",
-  "/home-hero-background.svg",
   "/apple-touch-icon.png",
   "/icon-192.png",
   "/icon-512.png",
